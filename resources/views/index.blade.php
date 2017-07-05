bcefd9776774a2d78fac0177380a183511e6b75b
Camilorc <camiloriffoc@gmail.com>
2017-07-04 20:23:36 -0400

    Proyecto Inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+	
+	<a class="btn btn-primary" href="#" role="button">boton 1</a></br></br>
+	<a class="btn btn-success" href="#" role="button">boton 2</a>
+
+	
+@endsection
